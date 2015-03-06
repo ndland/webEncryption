01579d56f130142d1b4e130f4e78752fcc3613e5
Nicholas Land <nland22@gmail.com>
2015-03-06 17:06:38 -0500

    Moved to a new package. More relevant to the project.

--- src/main/java/me/nickland/webEncryption/Application.java
+++ src/main/java/me/nickland/webEncryption/Application.java
@@ -1,4 +1,4 @@
-package hello;
+package me.nickland.webEncryption;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/me/nickland/webEncryption/WebEncryptionController.java
+++ src/main/java/me/nickland/webEncryption/WebEncryptionController.java
@@ -1,4 +1,4 @@
-package hello;
+package me.nickland.webEncryption;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
