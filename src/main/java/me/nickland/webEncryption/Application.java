919d56c196af129dcc5cbbac5b0f955c26b6d6f2
Nicholas Land <ndland@oakland.edu>
2015-03-19 16:54:24 -0400

    Form setup, passing data to controller.

--- src/main/java/me/nickland/webEncryption/Application.java
+++ src/main/java/me/nickland/webEncryption/Application.java
@@ -3,8 +3,10 @@ package me.nickland.webEncryption;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * Created by nland on 3/6/2015.
--- src/main/java/me/nickland/webEncryption/WebEncryptionController.java
+++ src/main/java/me/nickland/webEncryption/WebEncryptionController.java
@@ -3,7 +3,10 @@ package me.nickland.webEncryption;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.File;
 
 /**
  * Created by nland on 3/6/2015.
@@ -17,4 +20,13 @@ public class WebEncryptionController {
         model.addAttribute("message", "Hello, From Thymeleaf!");
         return "hello";
     }
+
+    @RequestMapping(value = "/processfile", method = RequestMethod.POST)
+    public String processInput(@RequestParam(value = "file", required = false) File file,
+                               @RequestParam(value = "password") String password, Model model) {
+        model.addAttribute("file", file);
+        model.addAttribute("password", password);
+        return "processedfile";
+    }
 }
+
