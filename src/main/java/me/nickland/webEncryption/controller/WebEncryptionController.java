e97b60b4b9a11ca227588fa1a70afb978919ec69
Nicholas Land <ndland@oakland.edu>
2015-04-08 23:19:58 -0400

    Remove unused imports.

--- src/main/java/me/nickland/webEncryption/controller/WebEncryptionController.java
+++ src/main/java/me/nickland/webEncryption/controller/WebEncryptionController.java
@@ -1,23 +1,13 @@
 package me.nickland.webEncryption.controller;
 
-import com.evernote.auth.EvernoteAuth;
-import com.evernote.auth.EvernoteService;
-import com.evernote.clients.ClientFactory;
-import com.evernote.clients.NoteStoreClient;
-import com.evernote.edam.error.EDAMSystemException;
-import com.evernote.edam.error.EDAMUserException;
-import com.evernote.edam.type.Notebook;
-import com.evernote.thrift.TException;
 import me.nickland.webEncryption.model.EvernoteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.File;
-import java.util.List;
 
 /**
  * Created by nland on 3/6/2015.
