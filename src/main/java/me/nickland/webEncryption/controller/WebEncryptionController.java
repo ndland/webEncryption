c410bd78f38bfc1bdf86516b1fd5db8c56c766bf
Mayce <mbvaraca@oakland.edu>
2015-04-12 15:32:49 -0400

    Added the JAR files: bcpkix-jdk15on-152.jar, bcprov-jdk15on-152.jar (Both from BouncyCastle), and itextpdf-5.5.5.jar (from iText).
    Updated the Controller to include the encrypting and decrypting capabilities. The EncryptionModel has both encrypting and decrypting. The file paths are specific to my (mayce's) paths, except for the source files since they grab directly from your computer.

--- src/main/java/me/nickland/webEncryption/controller/WebEncryptionController.java
+++ src/main/java/me/nickland/webEncryption/controller/WebEncryptionController.java
@@ -1,12 +1,19 @@
 package me.nickland.webEncryption.controller;
 
+import com.itextpdf.text.DocumentException;
+import me.nickland.webEncryption.model.EncryptionModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by nland on 3/6/2015.
@@ -23,19 +30,39 @@ public class WebEncryptionController {
 
     @RequestMapping(value = "/encryptFile", method = RequestMethod.POST)
     public String processInput(@RequestParam(value = "file", required = false) File file,
-                               @RequestParam(value = "password") String password, Model model) {
+                               @RequestParam(value = "password") String password, Model model) throws IOException, DocumentException {
         model.addAttribute("transaction", "Encrypted PDF");
         model.addAttribute("file", file);
         model.addAttribute("password", password);
+
+        //Create an object of the Encryption Model
+        EncryptionModel eM = new EncryptionModel();
+
+        //Send the file to be encrypted
+        //          SRC: Source of file to be encyrpted
+        //          DEST: Destination of new encrypted file to be saved
+        //          Password: to encrypt the file with
+        eM.encryptPdf(file.getPath(), "C:/Users/Missy/Desktop/" + file + "_Encrypted.pdf", password);
         return "processedfile";
+
     }
 
     @RequestMapping(value = "/decryptFile", method = RequestMethod.POST)
     public String decryptFile(@RequestParam(value = "file", required = true) File file,
-                              @RequestParam(value = "password", required = true) String password, Model model) {
+                              @RequestParam(value = "password", required = true) String password, Model model)throws IOException, DocumentException {
         model.addAttribute("transaction", "Decrypted PDF");
         model.addAttribute("file", file);
         model.addAttribute("password", password);
+
+        //Create an object of the Encryption Model
+        EncryptionModel eM = new EncryptionModel();
+
+        //Decrypt the file
+        //Sends in - SRC: source of file that is encrypted,
+        //          DEST: Decrypted file to be saved to
+        //          Password: Password used to encrypt file
+        eM.decryptPdf(file.getAbsolutePath(), "C:/Users/Missy/Desktop/" + file + "_Decrypted.pdf", password);
         return "processedfile";
     }
+
 }
--- src/main/java/me/nickland/webEncryption/model/EncryptionModel.java
+++ src/main/java/me/nickland/webEncryption/model/EncryptionModel.java
@@ -1,7 +1,40 @@
 package me.nickland.webEncryption.model;
 
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfEncryptor;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import javax.swing.text.Document;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Created by Nick on 4/7/15.
  */
 public class EncryptionModel {
+
+    public void encryptPdf(String src, String dest, String password) throws IOException, DocumentException {
+
+        PdfReader reader = new PdfReader(src);
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(dest));
+        //System.out.println("Encrypting File");
+        stamper.setEncryption(password.getBytes(), password.getBytes(),
+                PdfWriter.ALLOW_PRINTING, PdfWriter.ENCRYPTION_AES_128 | PdfWriter.DO_NOT_ENCRYPT_METADATA);
+        //System.out.println("yes");
+
+        stamper.close();
+        reader.close();
+    }
+
+    public void decryptPdf(String src, String dest, String password) throws IOException, DocumentException {
+
+        PdfReader reader = new PdfReader(src, password.getBytes());
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(dest));
+        stamper.close();
+        reader.close();
+    }
+
+
 }
